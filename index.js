a796841aa38274fd35aaa5425df434afb37039e8
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-09-09 20:37:53 -0400

    commit tree d35ba0f1d2fc95330acd409147b7d0db7e7bad4d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(array){
+    let found = array.find(element => element.result === "W");
+    if (found === undefined){
+        return undefined;
+    } else{
+        return found.year;
+    }
+   
+}
